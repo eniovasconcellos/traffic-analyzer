b8bd585f8ec2b5e8c11377239380d6bb292ca423
teopost <teopost@gmail.com>
2015-06-10 16:46:15 +0200

    Initial commit

---
+++ traffic-analyzer1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import cv2
+import time
+
+face_cascade = cv2.CascadeClassifier('./cascades/cars.xml')
+vc = cv2.VideoCapture('./media/cars1.avi')
+
+if vc.isOpened():
+    rval , frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    rval, frame = vc.read()
+    time.sleep(.1)
+
+
+    cv2.line(frame, (135,0),(135,250),(255,0,0),2)
+
+    # car detection.
+    cars = face_cascade.detectMultiScale(frame, 1.1, 2)
+
+    ncars = 0
+    for (x,y,w,h) in cars:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+        ncars = ncars + 1
+
+    # show result
+    cv2.imshow("Result",frame)
+    cv2.waitKey(1);
+vc.release()
